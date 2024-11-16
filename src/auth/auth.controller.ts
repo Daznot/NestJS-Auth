3a1c04e43f39e802f845205fcbb594490ba42261
Maksim Kharlamov <kharlamovmm@mail.ru>
2024-11-17 00:59:41 +0300

    register user

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,28 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post } from '@nestjs/common';
 import { LoginDTO, RegisterDTO } from './dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
+    constructor(private readonly authService: AuthService) {}
+
     @Post('register')
-    register(@Body() dto: RegisterDTO) {}
+    async register(@Body() dto: RegisterDTO) {
+        const user = await this.authService.register(dto);
+        if (!user) {
+            throw new BadRequestException(
+                `Неполучается зарегистрировать пользователя с данными: ${JSON.stringify(dto)}`,
+            );
+        }
+    }
 
     @Post('login')
-    login(@Body() dto: LoginDTO) {}
+    async login(@Body() dto: LoginDTO) {
+        const tokens = await this.authService.login(dto);
+        if (!tokens) {
+            throw new BadRequestException(`Не получается войти с данными: ${JSON.stringify(dto)}`);
+        }
+    }
 
     @Get('refresh')
     refreshTokens() {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,14 +1,56 @@
-import { Injectable } from '@nestjs/common';
-import { RegisterDTO } from './dto';
+import { Injectable, Logger, UnauthorizedException } from '@nestjs/common';
+import { LoginDTO, RegisterDTO } from './dto';
 import { UserService } from '@user/user.service';
+import { Tokens } from './interfaces';
+import { compareSync } from 'bcrypt';
+import { Token, User } from '@prisma/client';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from '@prisma/prisma.service';
+import { v4 } from 'uuid';
+import { add } from 'date-fns';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userService: UserService) {}
+    private readonly logger = new Logger(AuthService.name);
+    constructor(
+        private readonly userService: UserService,
+        private readonly jwtService: JwtService,
+        private readonly prismaService: PrismaService,
+    ) {}
 
     register(dto: RegisterDTO) {
         return this.userService.save(dto).catch((err) => {
+            this.logger.error(err);
             return null;
+        });
+    }
+
+    async login(dto: LoginDTO): Promise<Tokens> {
+        const user: User = await this.userService.findOne(dto.email).catch((err) => {
+            this.logger.error(err);
+            return null;
+        });
+        if (!user || compareSync(dto.password, user.password)) {
+            throw new UnauthorizedException('Неверный логин или пароль');
+        }
+
+        const accessToken = this.jwtService.sign({
+            id: user.id,
+            email: user.email,
+            roles: user.Role,
+        });
+
+        const refreshToken = await this.getRefreshToken(user.id);
+        return { accessToken, refreshToken };
+    }
+
+    private async getRefreshToken(userId: string): Promise<Token> {
+        return this.prismaService.token.create({
+            data: {
+                token: v4(),
+                exp: add(new Date(), { months: 1 }),
+                userId,
+            },
         });
     }
 }
