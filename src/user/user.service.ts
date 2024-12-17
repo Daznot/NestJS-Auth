0bf449223dcd2e2fb1a18f5301d318701607287f
Maksim Kharlamov <kharlamovmm@mail.ru>
2024-12-18 00:42:12 +0300

    roles gurd & cache

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,16 @@
 import { JwtPayload } from '@auth/interfaces';
-import { BadRequestException, ForbiddenException, Injectable } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { BadRequestException, ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { Role, User } from '@prisma/client';
 import { PrismaService } from '@prisma/prisma.service';
 import { genSaltSync, hashSync } from 'bcrypt';
 
 @Injectable()
 export class UserService {
-    constructor(private readonly prismaService: PrismaService) {}
+    constructor(
+        private readonly prismaService: PrismaService,
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+    ) {}
 
     async save(user: Partial<User>) {
         const use = await this.prismaService.user.findFirst({ where: { email: user.email } });
@@ -24,6 +28,10 @@ export class UserService {
     }
 
     async findOne(idOrMail: string) {
+        const cash = await this.cacheManager.get<User>(idOrMail);
+        if (!cash) {
+            await this.cacheManager.set(idOrMail);
+        }
         const user = await this.prismaService.user.findFirst({
             where: {
                 OR: [{ id: idOrMail }, { email: idOrMail }],
